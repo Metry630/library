84c2b1da1a02e114ec9857354f0bae78fc067758
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2022-08-24 15:43:31 +0700

    Add information other than title to cards

--- script.js
+++ script.js
@@ -15,9 +15,16 @@ function addBookToLib(newBook){
 function displayBooks(myLib){
     for(let i = 0; i < myLib.length; i++){
         const newCard = document.createElement('div')
+        const title = document.createElement('div')
+        const author = document.createElement('div')
+        const pages = document.createElement('div')
+        const readStatus = document.createElement('button')
+        title.textContent = myLib[i]['title']
+        author.textContent = myLib[i]['author']
+        pages.textContent = myLib[i]['pages'] + ' pages'
+        readStatus.textContent = (myLib[i]['readStatus'] ? 'Read' : 'Not read')
         newCard.classList.add('newCard')
-        console.log( myLib[i]['title']);
-        newCard.textContent = myLib[i]['title']
+        newCard.append(title, author, pages, readStatus)
         container.append(newCard)
     }
 }
